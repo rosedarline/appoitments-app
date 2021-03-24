ba888e208af2c4a58e3b335f0b116fa4ee9b94a6
Rose Adam <rosedarline@gmail.com>
2021-03-24 14:45:55 -0400

    Update Components/About

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <>
-      <p>Hello! I am Rose Adam. I created Natural Hairpedia to help those who are struggling to embrace their natural hair and want help finding the best products, treatments, and routines for their hair types. My goal is inspire you on your natural hair journey and to create a space for us to learn from each other. Please check out our blogs at <a href="https://naturalhairpedia.com/" target="_blank">Natural Hairpedia</a></p>
+      <p>Hello! I am Rose Adam. I created Natural Hairpedia to help those who are struggling to embrace their natural hair and want help finding the best products, treatments, and routines for their hair types. My goal is inspire you on your natural hair journey and to create a space for us to learn from each other. Please check out our blogs at <a href="https://naturalhairpedia.com/" target="_blank" rel="noreferrer">Natural Hairpedia</a></p>
 
       <Link to="/">Go Back</Link>
     </>
