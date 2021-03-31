e43fdcf3bc788719ecf51fdba797c51e06c09ab5
Rose Adam <rosedarline@gmail.com>
2021-03-30 20:01:17 -0400

    import Calendar, add day and time to state

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,18 @@
 import { useState } from "react";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
 
-const AddTask = ({ onAdd }) => {
+function AddTask({ onAdd }) {
   const [text, setText] = useState("");
-  const [day, setDay] = useState("");
+  const [name, setName] = useState("");
+  const [day, setDay] = useState(new Date());
+  const [time, setTime] = useState("");
   const [reminder, setReminder] = useState(false);
+
+  function onChange(nextValue) {
+    setDay(nextValue);
+  }
+
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -12,47 +21,76 @@ const AddTask = ({ onAdd }) => {
       alert("Please add a task");
       return;
     }
+    const timestampToString = JSON.stringify(day).slice(1, 11);
 
-    onAdd({ text, day, reminder });
+    onAdd({ text, name, timestampToString, time, reminder });
 
     setText("");
+    setName("");
     setDay("");
+    setTime("")
     setReminder(false);
   };
 
   return (
     <form className="add-form" onSubmit={onSubmit}>
       <div className="form-control">
-        <label>Appointment</label>
-        <input
-          type="text"
-          placeholder="Add Appointment"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
+          <label>Add Name</label>
+          <input
+            type="text"
+            placeholder="Add Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)} 
+          />
+      </div>  
+    
+       <div className="form-control">
+          <label>Add Service</label>
+          <input
+            type="text"
+            placeholder="Add Service"
+            value={text}
+            onChange={(e) => setText(e.target.value)} 
+          />
       </div>
-      <div className="form-control">
-        <label>Day & Time</label>
-        <input
-          type="text"
-          placeholder="Add Day & Time"
-          value={day}
-          onChange={(e) => setDay(e.target.value)}
-        />
+
+      <div className="form-control calendar">
+          <label>Select Date</label>
+      </div>
+      <div className="form-control calendar">
+          <Calendar 
+            onChange={onChange}
+            value={day}
+          />
+      </div>
+      <div className="form-control form-time">
+          <label>Add Time</label>
+          <select value={time} onChange={(e) => setTime(e.target.value)}>
+            <option value="9 AM" className="select-time">9 AM</option>
+            <option  value="10 AM" className="select-time">10 AM</option>
+            <option  value="11 AM" className="select-time">11 AM</option>
+            <option  value="11 AM" className="select-time">11 AM</option>
+            <option  value="12 PM" className="select-time">12 PM</option>
+            <option  value="1 PM" className="select-time">1 PM</option>
+            <option  value="2 PM" className="select-time">2 PM</option>
+            <option  value="3 PM" className="select-time">3 PM</option>
+            <option  value="4 PM" className="select-time">4 PM</option>
+          
+          </select>
       </div>
       <div className="form-control form-control-check">
-        <label>Add Reminder</label>
-        <input
-          type="checkbox"
-          checked={reminder}
-          value={reminder}
-          onChange={(e) => setReminder(e.currentTarget.checked)}
-        />
+          <label>Add Reminder</label>
+          <input
+            type="checkbox"
+            checked={reminder}
+            value={reminder}
+            onChange={(e) => setReminder(e.currentTarget.checked)} 
+          />
       </div>
 
       <input type="submit" value="Set Appointment" className="btn btn-block" />
     </form>
   );
-};
+}
 
 export default AddTask;
